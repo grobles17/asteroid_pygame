04728fe299769645555b2f488568bdc7e9f6a7ff
grobles17 <grobles_17@hotmail.com>
2024-12-14 00:14:34 +0100

    constants.py and game window setup

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import pygame
+from constants import *
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_WIDTH))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return 
+        #checks if user closes the window, and if true quits teh screen
+        screen.fill(color="black") #fills screen in solid black
+        pygame.display.flip() #refreshes the screen
+
 
 if __name__== "__main__":
     main()
