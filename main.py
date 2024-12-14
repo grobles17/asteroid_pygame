a8e732f178c0398f5295057f7734702f564cb71f
grobles17 <grobles_17@hotmail.com>
2024-12-14 01:15:28 +0100

    Added rotation to player

--- main.py
+++ main.py
@@ -4,24 +4,28 @@ from player import Player
 from circleshape import CircleShape
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_WIDTH))
     dt = 0 #delta time (time between frames)
     clock = pygame.time.Clock() #clock object
+    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
         #checks if user closes the window, and if true quits teh screen
+        
+        player.update(dt)
+
         screen.fill(color="black") #fills screen in solid black
-        player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
         player.draw(screen)
         pygame.display.flip() #refreshes the screen
+        
+
+
         dt = clock.tick(60)/1000 #uses clock objet to set max framerate to 60fps
         #tick method returns the delta time in milliseconds -> store it in dt in seconds
-
 
 if __name__== "__main__":
     main()
--- player.py
+++ player.py
@@ -19,3 +19,15 @@ class Player(CircleShape): #inherits from CircleShape clas
     def draw(self, screen):
         pygame.draw.polygon(screen, color="white", points=self.triangle(), width=2)
     
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        """Checks if keys a or d are pressed to rotate the player left or right
+        Takes 1 argument as int/float ("dt")"""
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
